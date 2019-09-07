11265c73b55aca41a08bcdbd81438c336fe54657
Erick Pham <pqtoan220496@gmail.com>
2019-09-07 15:52:09 +0700

    init

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  collectCoverageFrom: [
+    'app/**/*.{js,jsx}',
+    '!app/**/*.test.{js,jsx}',
+    '!app/*/RbGenerated*/*.{js,jsx}',
+    '!app/app.js',
+    '!app/global-styles.js',
+    '!app/*/*/Loadable.{js,jsx}',
+    '!app/assets/**/*.js',
+    '!app/utils/loadable.{js,jsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
+  moduleDirectories: ['node_modules', 'app'],
+  moduleNameMapper: {
+    '.*\\.(css|less|styl|scss|sass)$': '<rootDir>/internals/mocks/cssModule.js',
+    '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/internals/mocks/image.js',
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/internals/testing/test-bundler.js',
+    'react-testing-library/cleanup-after-each',
+  ],
+  setupFiles: ['raf/polyfill', '<rootDir>/jest.setup.js'],
+  testRegex: 'tests/.*\\.test\\.js$',
+  snapshotSerializers: [],
+};
